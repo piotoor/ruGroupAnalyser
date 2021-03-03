ca8eae5eeea455775b9930bc274e5b6924585d2c
Piotr Kozka <piotoor@gmail.com>
2021-03-03 22:37:52 +0100

    Added ruCubeTest::settersGettersTest

--- tst/ruCube_test.cpp
+++ tst/ruCube_test.cpp
@@ -12,3 +12,15 @@ TEST(ruCubeTest, initialStateTest) {
     ASSERT_TRUE (cube.isInDomino());
 }
 
+TEST(ruCubeTest, settersGettersTest) {
+    ruCube cube;
+    cube.setEdges(076543210);
+    cube.setCorners(01511413121110);
+    ASSERT_EQ (076543210, cube.getEdges());
+    ASSERT_EQ (01511413121110, cube.getCorners());
+
+    cube.setCube(001234765, 01011112151413);
+    ASSERT_EQ (001234765, cube.getEdges());
+    ASSERT_EQ (01011112151413, cube.getCorners());
+}
+
