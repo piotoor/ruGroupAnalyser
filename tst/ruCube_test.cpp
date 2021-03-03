329dd5a4117d5168de4fe808649cd9d72df8044f
Piotr Kozka <piotoor@gmail.com>
2021-03-03 22:37:23 +0100

    Added ruCubeTest::initialStateTest

---
+++ tst/ruCube_test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "ruCube.h"
+
+
+
+TEST(ruCubeTest, initialStateTest) {
+    ruCube cube;
+
+    ASSERT_EQ (ruCube::solvedEdges, cube.getEdges());
+    ASSERT_EQ (ruCube::solvedCorners, cube.getCorners());
+    ASSERT_TRUE (cube.isSolved(ruCube::allEdgesMask, ruCube::allCornersMask));
+    ASSERT_TRUE (cube.isInDomino());
+}
+
