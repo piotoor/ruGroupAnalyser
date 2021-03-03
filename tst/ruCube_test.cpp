8fe5cd6708a098caf23d5a688748e76db3907f83
Piotr Kozka <piotoor@gmail.com>
2021-03-03 22:38:42 +0100

    Added ruCubeTest::singleTurnTest

--- tst/ruCube_test.cpp
+++ tst/ruCube_test.cpp
@@ -33,4 +33,37 @@ TEST(ruCubeTest, cubeStateResetTest) {
     ASSERT_TRUE (cube.isSolved(ruCube::allEdgesMask, ruCube::allCornersMask));
 }
 
+TEST(ruCubeTest, singleTurnTest) {
+	/*
+	 *	0 - R
+	 * 	1 - R2
+	 * 	2 - R'
+	 *  3 - U
+	 *  4 - U2
+	 *  5 - U'
+	 *
+	 * const unsigned long int edgesSolved = 00123456;
+	 * const unsigned long int cornersSolved = 0000102030405;
+	 */
+    ruCube cube;
+
+	cube.turn(0); // R
+	ASSERT_EQ(00126345, cube.getEdges());
+	ASSERT_EQ(0251112402344, cube.getCorners());
+	cube.turn(3); // U
+	ASSERT_EQ(06012345, cube.getEdges());
+	ASSERT_EQ(0402511122344, cube.getCorners());
+	cube.turn(1); // R2
+	ASSERT_EQ(06014523, cube.getEdges());
+	ASSERT_EQ(0232511444012, cube.getCorners());
+	cube.turn(4); // U2
+	ASSERT_EQ(01460523, cube.getEdges());
+	ASSERT_EQ(0114423254012, cube.getCorners());
+	cube.turn(2); // R'
+	ASSERT_EQ(01465230, cube.getEdges());
+	ASSERT_EQ(0454423202241, cube.getCorners());
+	cube.turn(5); // U'
+	ASSERT_EQ(04651230, cube.getEdges());
+	ASSERT_EQ(0442320452241, cube.getCorners());
+}
 
