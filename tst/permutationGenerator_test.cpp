f6582624464d7e2a703b9f95b6858ada170e283e
Piotr Kozka <piotoor@gmail.com>
2021-03-07 22:21:50 +0100

    Added permutationGeneratorTest::generatePermutationsWithLockedPiecesTest

--- tst/permutationGenerator_test.cpp
+++ tst/permutationGenerator_test.cpp
@@ -58,3 +58,108 @@ TEST(permutationGeneratorTest, generatePermutationsTest) {
         ASSERT_EQ(expectedPerms[i], perms[i]);
     }
 }
+
+
+TEST(permutationGeneratorTest, generatePermutationsWithLockedPiecesTest) {
+    permutationGenerator gen;
+    std::vector<int8_t> lockedPieces = {3, 4, 5};
+    auto perms = gen.generatePermutationsWithLockedPieces(6, lockedPieces);
+    std::vector<std::vector<int8_t>> expectedPerms = {
+        { 0, 1, 2 },
+        { 0, 2, 1 },
+        { 1, 0, 2 },
+        { 1, 2, 0 },
+        { 2, 0, 1 },
+        { 2, 1, 0 }
+    };
+
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+    lockedPieces = {1};
+    perms = gen.generatePermutationsWithLockedPieces(3, lockedPieces);
+    expectedPerms = {
+        { 0, 2 },
+        { 2, 0 }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+    lockedPieces = {1, 2, 3};
+    perms = gen.generatePermutationsWithLockedPieces(6, lockedPieces);
+    expectedPerms = {
+        { 0, 4, 5 },
+        { 0, 5, 4 },
+        { 4, 0, 5 },
+        { 4, 5, 0 },
+        { 5, 0, 4 },
+        { 5, 4, 0 }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+    lockedPieces = {1, 2, 3, 1, 2, 3};
+    perms = gen.generatePermutationsWithLockedPieces(6, lockedPieces);
+    expectedPerms = {
+        { 0, 4, 5 },
+        { 0, 5, 4 },
+        { 4, 0, 5 },
+        { 4, 5, 0 },
+        { 5, 0, 4 },
+        { 5, 4, 0 }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+
+    lockedPieces = {1, 2, 3};
+    perms = gen.generatePermutationsWithLockedPieces(4, lockedPieces);
+    expectedPerms = {
+        { 0 }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+    lockedPieces = {0, 1, 2, 3};
+    perms = gen.generatePermutationsWithLockedPieces(4, lockedPieces);
+    expectedPerms = {
+        {  }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+
+
+    lockedPieces = {1, 2, 3, 5, 9, 8, 11};
+    perms = gen.generatePermutationsWithLockedPieces(4, lockedPieces);
+    expectedPerms = {
+        { 0 }
+    };
+    ASSERT_EQ(expectedPerms.size(), perms.size());
+    for (int i = 0; i < expectedPerms.size(); ++i) {
+        ASSERT_EQ(expectedPerms[i], perms[i]);
+    }
+
+}
