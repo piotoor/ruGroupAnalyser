819aa035befae510d50dcb8384a8440456c2f239
Piotr Kozka <piotoor@gmail.com>
2021-03-17 23:28:08 +0100

    Added orientationGeneratorTest, generateOrientationtTest

--- tst/orientationGenerator_test.cpp
+++ tst/orientationGenerator_test.cpp
@@ -2,9 +2,185 @@
 #include "orientationGenerator.h"
 
 #include <vector>
+#include <iostream>
+using namespace std;
 
 
 TEST(orientationGeneratorTest, generateOrientationtTest) {
     orientationGenerator gen;
-    ASSERT_EQ(1, 2);
+
+    auto orients = gen.generateOrientations(0);
+    std::vector<std::vector<int8_t>> expectedOrients = {
+        { }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
+
+
+    orients = gen.generateOrientations(1);
+    expectedOrients = {
+        { 0 }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
+
+
+    orients = gen.generateOrientations(2);
+    expectedOrients = {
+        { 0, 0 },
+        { 1, 2 },
+        { 2, 1 }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
+
+
+    orients = gen.generateOrientations(3);
+    expectedOrients = {
+        { 0, 0, 0 },
+        { 0, 1, 2 },
+        { 0, 2, 1 },
+        { 1, 0, 2 },
+        { 1, 1, 1 },
+        { 1, 2, 0 },
+        { 2, 0, 1 },
+        { 2, 1, 0 },
+        { 2, 2, 2 }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
+
+
+    orients = gen.generateOrientations(4);
+    expectedOrients = {
+        { 0, 0, 0, 0 },
+        { 0, 0, 1, 2 },
+        { 0, 0, 2, 1 },
+        { 0, 1, 0, 2 },
+        { 0, 1, 1, 1 },
+        { 0, 1, 2, 0 },
+        { 0, 2, 0, 1 },
+        { 0, 2, 1, 0 },
+        { 0, 2, 2, 2 },
+        { 1, 0, 0, 2 },
+        { 1, 0, 1, 1 },
+        { 1, 0, 2, 0 },
+        { 1, 1, 0, 1 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 2, 2 },
+        { 1, 2, 0, 0 },
+        { 1, 2, 1, 2 },
+        { 1, 2, 2, 1 },
+        { 2, 0, 0, 1 },
+        { 2, 0, 1, 0 },
+        { 2, 0, 2, 2 },
+        { 2, 1, 0, 0 },
+        { 2, 1, 1, 2 },
+        { 2, 1, 2, 1 },
+        { 2, 2, 0, 2 },
+        { 2, 2, 1, 1 },
+        { 2, 2, 2, 0 }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
+
+
+    orients = gen.generateOrientations(5);
+    expectedOrients = {
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 2 },
+        { 0, 0, 0, 2, 1 },
+        { 0, 0, 1, 0, 2 },
+        { 0, 0, 1, 1, 1 },
+        { 0, 0, 1, 2, 0 },
+        { 0, 0, 2, 0, 1 },
+        { 0, 0, 2, 1, 0 },
+        { 0, 0, 2, 2, 2 },
+        { 0, 1, 0, 0, 2 },
+        { 0, 1, 0, 1, 1 },
+        { 0, 1, 0, 2, 0 },
+        { 0, 1, 1, 0, 1 },
+        { 0, 1, 1, 1, 0 },
+        { 0, 1, 1, 2, 2 },
+        { 0, 1, 2, 0, 0 },
+        { 0, 1, 2, 1, 2 },
+        { 0, 1, 2, 2, 1 },
+        { 0, 2, 0, 0, 1 },
+        { 0, 2, 0, 1, 0 },
+        { 0, 2, 0, 2, 2 },
+        { 0, 2, 1, 0, 0 },
+        { 0, 2, 1, 1, 2 },
+        { 0, 2, 1, 2, 1 },
+        { 0, 2, 2, 0, 2 },
+        { 0, 2, 2, 1, 1 },
+        { 0, 2, 2, 2, 0 },
+        { 1, 0, 0, 0, 2 },
+        { 1, 0, 0, 1, 1 },
+        { 1, 0, 0, 2, 0 },
+        { 1, 0, 1, 0, 1 },
+        { 1, 0, 1, 1, 0 },
+        { 1, 0, 1, 2, 2 },
+        { 1, 0, 2, 0, 0 },
+        { 1, 0, 2, 1, 2 },
+        { 1, 0, 2, 2, 1 },
+        { 1, 1, 0, 0, 1 },
+        { 1, 1, 0, 1, 0 },
+        { 1, 1, 0, 2, 2 },
+        { 1, 1, 1, 0, 0 },
+        { 1, 1, 1, 1, 2 },
+        { 1, 1, 1, 2, 1 },
+        { 1, 1, 2, 0, 2 },
+        { 1, 1, 2, 1, 1 },
+        { 1, 1, 2, 2, 0 },
+        { 1, 2, 0, 0, 0 },
+        { 1, 2, 0, 1, 2 },
+        { 1, 2, 0, 2, 1 },
+        { 1, 2, 1, 0, 2 },
+        { 1, 2, 1, 1, 1 },
+        { 1, 2, 1, 2, 0 },
+        { 1, 2, 2, 0, 1 },
+        { 1, 2, 2, 1, 0 },
+        { 1, 2, 2, 2, 2 },
+        { 2, 0, 0, 0, 1 },
+        { 2, 0, 0, 1, 0 },
+        { 2, 0, 0, 2, 2 },
+        { 2, 0, 1, 0, 0 },
+        { 2, 0, 1, 1, 2 },
+        { 2, 0, 1, 2, 1 },
+        { 2, 0, 2, 0, 2 },
+        { 2, 0, 2, 1, 1 },
+        { 2, 0, 2, 2, 0 },
+        { 2, 1, 0, 0, 0 },
+        { 2, 1, 0, 1, 2 },
+        { 2, 1, 0, 2, 1 },
+        { 2, 1, 1, 0, 2 },
+        { 2, 1, 1, 1, 1 },
+        { 2, 1, 1, 2, 0 },
+        { 2, 1, 2, 0, 1 },
+        { 2, 1, 2, 1, 0 },
+        { 2, 1, 2, 2, 2 },
+        { 2, 2, 0, 0, 2 },
+        { 2, 2, 0, 1, 1 },
+        { 2, 2, 0, 2, 0 },
+        { 2, 2, 1, 0, 1 },
+        { 2, 2, 1, 1, 0 },
+        { 2, 2, 1, 2, 2 },
+        { 2, 2, 2, 0, 0 },
+        { 2, 2, 2, 1, 2 },
+        { 2, 2, 2, 2, 1 }
+    };
+    ASSERT_EQ(expectedOrients.size(), orients.size());
+    for (int i = 0; i < expectedOrients.size(); ++i) {
+        ASSERT_EQ(expectedOrients[i], orients[i]);
+    }
 }
