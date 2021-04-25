8f16d01b8b38a72f7e9dc31d8e7c5674c1c72f46
Piotr Kozka <piotoor@gmail.com>
2021-04-26 01:13:41 +0200

    Replaced simple ruCubeSolverTest::benchmarkTest with more advanced uCubeSolverPerformanceTests fixture class

--- tst/ruCubeSolver_test.cpp
+++ tst/ruCubeSolver_test.cpp
@@ -224,32 +224,138 @@ TEST(ruCubeSolverTest, multipleScramblesTest) {
 
 }
 
-TEST (ruCubeSolverTest, benchmarkTest) {
-    ruCube cube;
-    std::vector<uint8_t> scr { R2, U, R, Ui, R2, U, R, U, R, Ui, R2, U2, R, U2, Ri, U2, R, U, Ri, U };
-    cube.scramble(scr);
+class ruCubeSolverPerformanceTests : public ::testing::Test
+{
+    protected:
+        virtual void SetUp() {
 
-    const uint8_t minLength = 1;
-    const uint8_t maxLength = 20;
-    const uint8_t numOfSolves = 10;
+        }
 
-    ruCubeSolver solver(minLength, maxLength, numOfSolves);
+        virtual void TearDown() {
 
-    const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+        }
 
-    solver.solve(&cube, true);
-    auto solutions = solver.getSolutionsAsVectors();
-    for (const auto &sol: solutions) {
-        std::copy(begin(sol), end(sol), std::ostream_iterator<int>(std::cout, " "));
-        std::cout << std::endl;
-        cube.reset();
-        cube.scramble(scr);
-        cube.scramble(sol);
-        ASSERT_TRUE(cube.isSolved());
+        void setupSolver() {
+            minLength = 1;
+            maxLength = 20;
+            numOfSolves = 10;
+            scramble = { R2, U, R, Ui, R2, U, R, U, R, Ui, R2, U2, R, U2, Ri, U2, R, U, Ri, U };
+            solver.configure(minLength, maxLength, numOfSolves);
+        }
+
+        void solveSingleThread() {
+            std::cout << "solveSingleThread... ";
+            cube.reset();
+            cube.scramble(scramble);
+
+            const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+
+            solver.solve(&cube);
+            solutions = solver.getSolutionsAsVectors();
+
+            const auto stop = std::chrono::steady_clock::now();
+            std::chrono::duration<double> diff = stop - start;
+            singleThreadTime = diff;
+            verifySolutions();
+            std::cout << std::setw(19) << "DONE" << std::endl;
+        }
+
+        void solveMultiThread(uint8_t multiThreadingThreshold) {
+            std::cout << "solveMultiThread of threshold " << std::setw(2) << static_cast<int>(multiThreadingThreshold) << "... ";
+            cube.scramble(scramble);
+
+            const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+
+            solver.solve(&cube, true, multiThreadingThreshold);
+            solutions = solver.getSolutionsAsVectors();
+
+            const auto stop = std::chrono::steady_clock::now();
+            std::chrono::duration<double> diff = stop - start;
+            multiThreadingThresholdsTimes[multiThreadingThreshold] = diff;
+
+            verifySolutions();
+            std::cout << "DONE" << std::endl;
+        }
+
+        void verifySolutions() {
+            for (const auto &sol: solutions) {
+    //            std::copy(begin(sol), end(sol), std::ostream_iterator<int>(std::cout, " "));
+    //            std::cout << std::endl;
+                cube.reset();
+                cube.scramble(scramble);
+                cube.scramble(sol);
+                ASSERT_TRUE(cube.isSolved());
+            }
+        }
+
+        void printReport() {
+            using namespace std::literals;
+            std::cout << std::endl;
+            std::cout << "+---------+-- Report -+-----------+" << std::endl;
+            std::cout << "| threads | threshold |" << std::setw(11) << " time [ms] " << "|" << std::endl;
+            std::cout << "+---------+-----------+-----------+" << std::endl;
+            std::cout << "| " << std::setw(7) << 1 << " | " << std::setw(9) << "N/A" << " | " << std::setw(9) << singleThreadTime / 1ms << " |" << std::endl;
+
+            for (const auto &[k, v]: multiThreadingThresholdsTimes) {
+                std::cout << "| " << std::setw(7) << 6 << " | " << std::setw(9) << static_cast<int>(k) << " | " << std::setw(9) << v / 1ms << " |" << std::endl;
+            }
+
+            std::cout << "+---------+-----------+-----------+" << std::endl;
+            std::cout << std::endl;
+        }
+
+
+        ruCubeSolver solver;
+        ruCube cube;
+        std::map<uint8_t, std::chrono::duration<double>> multiThreadingThresholdsTimes;
+        std::chrono::duration<double> singleThreadTime;
+
+        std::vector<std::vector<uint8_t>> solutions;
+        std::vector<uint8_t> scramble;
+
+        uint8_t minLength;
+        uint8_t maxLength;
+        uint8_t numOfSolves;
+
+};
+
+TEST_F(ruCubeSolverPerformanceTests, MultithreadingTest) {
+    setupSolver();
+    solveSingleThread();
+
+    for (uint8_t threshold = minLength; threshold < maxLength; ++threshold) {
+        solveMultiThread(threshold);
     }
 
-    using namespace std::literals;
-    const auto stop = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff = stop - start;
-    std::cout << diff / 1ms << "ms" << std::endl;
+    printReport();
 }
+
+//TEST (ruCubeSolverTest, benchmarkTest) {
+//    ruCube cube;
+//    std::vector<uint8_t> scr { R2, U, R, Ui, R2, U, R, U, R, Ui, R2, U2, R, U2, Ri, U2, R, U, Ri, U };
+//    cube.scramble(scr);
+//
+//    const uint8_t minLength = 1;
+//    const uint8_t maxLength = 20;
+//    const uint8_t numOfSolves = 10;
+//
+//    ruCubeSolver solver(minLength, maxLength, numOfSolves);
+//
+//    const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+//
+//    solver.solve(&cube, true);
+//    auto solutions = solver.getSolutionsAsVectors();
+//    for (const auto &sol: solutions) {
+//        std::copy(begin(sol), end(sol), std::ostream_iterator<int>(std::cout, " "));
+//        std::cout << std::endl;
+//        cube.reset();
+//        cube.scramble(scr);
+//        cube.scramble(sol);
+//        ASSERT_TRUE(cube.isSolved());
+//    }
+//
+//    using namespace std::literals;
+//    const auto stop = std::chrono::steady_clock::now();
+//    std::chrono::duration<double> diff = stop - start;
+//    std::cout << diff / 1ms << "ms" << std::endl;
+//}
