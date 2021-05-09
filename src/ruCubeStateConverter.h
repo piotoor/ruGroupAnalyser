bbc38e0e9ac899c39da2d81c65c6d8a0c01f0a36
Piotr Kozka <piotoor@gmail.com>
2021-05-09 15:24:51 +0200

    Implemented ruCubeStateConverter::intEdgesToEdgesLexIndex. Working. To be optimized

--- src/ruCubeStateConverter.h
+++ src/ruCubeStateConverter.h
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 #include <vector>
+#include <array>
+#include <bitset>
 
 class ruCubeStateConverter
 {
@@ -13,12 +15,16 @@ class ruCubeStateConverter
         virtual ~ruCubeStateConverter();
         uint64_t vectCornersToInt(const std::vector<int8_t> &perm, const std::vector<int8_t> &orient) const;
         uint32_t vectEdgesToInt(const std::vector<int8_t> &perm) const;
-        uint16_t intEdgesToEdgesLexIndex(const uint32_t) const;
-        uint16_t intCornersToCornersPermLexIndex(const uint64_t) const;
-        uint16_t intCornersToCornersOrientLexIndex(const uint64_t) const;
+        uint16_t intEdgesToEdgesLexIndex(const uint32_t edges) const;
+        uint16_t intCornersToCornersPermLexIndex(const uint64_t corners) const;
+        uint16_t intCornersToCornersOrientLexIndex(const uint64_t corners) const;
 
     protected:
+//        static std::array<uint8_t, maxNumOfPieces> onesCountLookup;
+//        static std::array<uint8_t, maxNumOfPieces> factorialLookup;
 
+        static inline const uint8_t maxNumOfPieces = 7;
+        static inline const uint8_t numOfEdges = 7;
     private:
 };
 
