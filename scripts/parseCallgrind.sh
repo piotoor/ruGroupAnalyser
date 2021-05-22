7941e5cdc7a4a81e7601b7cc1fdcf388c49a2ede
Piotr Kozka <piotoor@gmail.com>
2021-05-22 15:33:37 +0200

    Scripts cleanup

--- scripts/parseCallgrind.sh
+++ scripts/parseCallgrind.sh
@@ -4,5 +4,3 @@ for file in `ls -tU callgrind*.out`; do
   callgrind_annotate $file | grep -iE "function|index" | grep -vi test
   echo
 done
-# callgrind_annotate $1 | grep -i function
-# callgrind_annotate $1 | grep -i index | grep -vi test
