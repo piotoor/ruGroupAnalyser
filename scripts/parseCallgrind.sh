5dda0d50e68c8783ae1cd792bbca34eb70e08c24
Piotr Kozka <piotoor@gmail.com>
2021-05-24 23:13:35 +0200

    parseCallgrind.sh script can now filter any function name, provided as an argument

--- scripts/parseCallgrind.sh
+++ scripts/parseCallgrind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in `ls -tU callgrind*.out`; do
   echo "--------------" $file  "--------------"
-  callgrind_annotate $file | grep -iE "function|index" | grep -vi test
+  callgrind_annotate $file | grep -iE "function|$1" | grep -vi test
   echo
 done
