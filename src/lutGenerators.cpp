b4bef718e5d37c4aa3d01b13c8164f979f162e2d
Piotr Kozka <piotoor@gmail.com>
2021-06-01 01:29:55 +0200

    Added generateCornersPermMoveMap and generateCornersOrientMoveMap to lutGenerators

--- src/lutGenerators.cpp
+++ src/lutGenerators.cpp
@@ -23,4 +23,40 @@ namespace lutGenerators {
 
         return ans;
     }
+
+    std::array<std::array<uint16_t, noOfTurns>, noOfCornersPermutations> generateCornersPermMoveMap () {
+        std::array<std::array<uint16_t, noOfTurns>, noOfCornersPermutations> ans {};
+        auto cube = ruCubeFactory::createCube(ruCubeFactory::ruCubeType::ruCube);
+        ruCubeStateConverter converter;
+
+        for (uint16_t cornersPerm = 0; cornersPerm < noOfCornersPermutations; ++cornersPerm) {
+            cube->setCorners(converter.lexIndexCornersToIntCorners(cornersPerm, ruCube::solvedLexIndexCornersOrient));
+
+            for (uint8_t t = R; t <= Ui; ++t) {
+                cube->turn(t);
+                ans[cornersPerm][t] = converter.intCornersToLexIndexCornersPerm(cube->getCorners());
+                cube->inverseTurn(t);
+            }
+        }
+
+        return ans;
+    }
+
+    std::array<std::array<uint16_t, noOfTurns>, noOfCornersOrientations> generateCornersOrientMoveMap () {
+        std::array<std::array<uint16_t, noOfTurns>, noOfCornersOrientations> ans {};
+        auto cube = ruCubeFactory::createCube(ruCubeFactory::ruCubeType::ruCube);
+        ruCubeStateConverter converter;
+
+        for (uint16_t cornersOrient = 0; cornersOrient < noOfCornersOrientations; ++cornersOrient) {
+            cube->setCorners(converter.lexIndexCornersToIntCorners(ruCube::solvedLexIndexCornersPerm, cornersOrient));
+
+            for (uint8_t t = R; t <= Ui; ++t) {
+                cube->turn(t);
+                ans[cornersOrient][t] = converter.intCornersToLexIndexCornersOrient(cube->getCorners());
+                cube->inverseTurn(t);
+            }
+        }
+
+        return ans;
+    }
 }
--- src/lutGenerators.h
+++ src/lutGenerators.h
@@ -4,8 +4,12 @@
 namespace lutGenerators {
     inline static const uint8_t noOfTurns = 6;
     inline static const uint16_t noOfEdgesPermutations = 5040;
+    inline static const uint16_t noOfCornersPermutations = 720;
+    inline static const uint16_t noOfCornersOrientations = 729;
 
     std::array<std::array<uint16_t, noOfTurns>, noOfEdgesPermutations> generateEdgesPermMoveMap ();
+    std::array<std::array<uint16_t, noOfTurns>, noOfCornersPermutations> generateCornersPermMoveMap ();
+    std::array<std::array<uint16_t, noOfTurns>, noOfCornersOrientations> generateCornersOrientMoveMap ();
 }
 
 
