af3c63d87a3ef8612c6490beaffdadfe1913c369
Piotr Kozka <piotoor@gmail.com>
2021-07-06 00:57:57 +0200

    Implemented missing code responsible for ruLutCube creation

--- src/ruCubeFactory.cpp
+++ src/ruCubeFactory.cpp
@@ -7,9 +7,7 @@ namespace ruCubeFactory {
         case ruCubeType::ruCube:
                 return std::make_unique<ruCube>();
             case ruCubeType::ruLutCube:
-                // generate lut table first time it gets there
-                // inject the dependency to lut cube
-                //return std::make_unique<ruLutCube>();
+                return std::make_unique<ruLutCube>();
             default:
                 throw ruCubeFactoryException();
         }
