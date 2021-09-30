8865996dcdc2f6c8ccb87934fe2250472899ea72
Piotr Kozka <piotoor@gmail.com>
2021-10-01 00:07:33 +0200

    Bugfix in ruCubeSingleSolveHandler::configure prototype. Added default parameters

--- src/ruCubeSingleSolveHandler.h
+++ src/ruCubeSingleSolveHandler.h
@@ -18,14 +18,14 @@ class ruCubeSingleSolveHandler
                                     uint32_t edgesMask = ruBaseCube::allEdgesMask,
                                     uint64_t cornersMask = ruBaseCube::allCornersMask);
         virtual ~ruCubeSingleSolveHandler();
-        void configure( uint8_t minLength,
-                        uint8_t maxLength,
-                        uint8_t maxNumOfSolutions,
-                        bool headers,
-                        bool lineNumbers,
-                        bool fixedWidthMoves,
-                        uint32_t edgesMask,
-                        uint64_t cornersMask);
+        void configure( uint8_t minLength = 0,
+                        uint8_t maxLength = 20,
+                        uint8_t maxNumOfSolutions = 1,
+                        bool headers = false,
+                        bool lineNumbers = false,
+                        bool fixedWidthMoves = false ,
+                        uint32_t edgesMask = ruBaseCube::allEdgesMask,
+                        uint64_t cornersMask = ruBaseCube::allCornersMask);
 
         void solve(ruLutCube cube);
         std::string getReport();
