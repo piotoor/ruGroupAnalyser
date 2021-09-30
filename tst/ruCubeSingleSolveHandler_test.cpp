0dcf2e8cdcbcc2e041203b95d1f1722b3abcf7d4
Piotr Kozka <piotoor@gmail.com>
2021-09-30 23:55:22 +0200

    Implemented ruCubeSingleSolveHandlerTest::getReportDefaultTest and ruCubeSingleSolveHandlerTest::getReportMaxNumOfSolutionsMinLengthMaxLengthTest

--- tst/ruCubeSingleSolveHandler_test.cpp
+++ tst/ruCubeSingleSolveHandler_test.cpp
@@ -1,21 +1,56 @@
 #include "gtest/gtest.h"
 #include "ruCubeSingleSolveHandler.h"
+#include "ruCubeSingleSolveInputParser.h"
 #include <vector>
 #include <iostream>
+#include <string>
 
+TEST(ruCubeSingleSolveHandlerTest, getReportDefaultTest) {
+    ruCubeSingleSolveHandler handler;
+    ruCubeSingleSolveInputParser parser;
 
-TEST(ruCubeSingleSolveHandlerTest, ScrambleNoFlagsTest) {
+    std::vector<std::string> scrambles {
+        "R",
+        "R U",
+        "R U R",
+        "R U R' U R U2 R'"
+    };
 
+    std::vector<std::string> expectedReports {
+        "R'\n",
+        "U' R'\n",
+        "R' U' R'\n",
+        "R U2 R' U' R U' R'\n"
+    };
+
+    for (uint8_t i = 0; i < size(scrambles); ++i) {
+        auto cube = parser.getCubeFromScramble(scrambles[i]);
+        handler.solve(cube);
+        ASSERT_EQ(expectedReports[i], handler.getReport());
+    }
 }
 
-TEST(ruCubeSingleSolveHandlerTest, ScrambleWithFlagsTest) {
+TEST(ruCubeSingleSolveHandlerTest, getReportMaxNumOfSolutionsMinLengthMaxLengthTest) {
+    ruCubeSingleSolveHandler handler(5, 15, 2);
+    ruCubeSingleSolveInputParser parser;
 
+    std::vector<std::string> scrambles {
+        "R",
+        "R U R' U R U2 R'"
+    };
+
+    std::vector<std::string> expectedReports {
+        "R U2 R2 U2 R2 U2 R2 U2 R2 U2 R2 U2\n"
+        "U2 R2 U2 R2 U2 R2 U2 R2 U2 R2 U2 R\n",
+
+        "R U2 R' U' R U' R'\n"
+        "R2 U2 R' U2 R' U R U2 R U2 R' U R'\n"
+    };
+
+    for (uint8_t i = 0; i < size(scrambles); ++i) {
+        auto cube = parser.getCubeFromScramble(scrambles[i]);
+        handler.solve(cube);
+        ASSERT_EQ(expectedReports[i], handler.getReport());
+    }
 }
 
-TEST(ruCubeSingleSolveHandlerTest, StateNoFlagsTest) {
-
-}
-
-TEST(ruCubeSingleSolveHandlerTest, StateWithFlagsTest) {
-
-}
