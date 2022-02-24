8df26b438ceb96a37d1d277503afd47f05e63f8a
Piotr Kozka <piotoor@gmail.com>
2022-02-24 01:39:38 +0100

    Defaulted ctor and dtor in ruLutCubeQueue class. Fixed order of includes

--- src/ruLutCubeQueue.h
+++ src/ruLutCubeQueue.h
@@ -1,16 +1,16 @@
 #ifndef RULUTCUBEQUEUE_H
 #define RULUTCUBEQUEUE_H
+
+#include "ruCube.h"
 #include <queue>
 #include <mutex>
 #include <condition_variable>
 #include <optional>
 
-#include "ruCube.h"
-
 class ruLutCubeQueue {
     public:
-        ruLutCubeQueue();
-        virtual ~ruLutCubeQueue();
+        ruLutCubeQueue() = default;
+        virtual ~ruLutCubeQueue() = default;
         void push(ruLutCube cube);
         std::optional<ruLutCube> pop();
         bool isEmpty();
@@ -21,7 +21,7 @@ class ruLutCubeQueue {
         std::mutex queue_mutex;
         std::condition_variable condition;
 
-        bool stopped;
+        bool stopped = false;
 };
 
 #endif // RULUTCUBEQUEUE_H
