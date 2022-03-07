295a900e1cc35fc1dd34a9d7abc8fb0e4ccb45b6
Piotr Kozka <piotoor@gmail.com>
2022-03-08 00:29:55 +0100

    ruCubeStateParserTest::correctStatesTest is now implemented as a value-parameterized ruCubeStateParserCorrectStatesTest fixture

--- tst/ruCubeStateParser_tst.cpp
+++ tst/ruCubeStateParser_tst.cpp
@@ -1,36 +1,63 @@
 #include "gtest/gtest.h"
 #include "ruCubeStateParser.h"
 #include "ruException.h"
+#include "testCustomDefinitions.h"
 #include <vector>
 #include <string>
 
 // cp, co, ep
 using ruCubeStateVect = std::tuple<cornersArray, cornersArray, edgesArray>;
 
-TEST(ruCubeStateParserTest, correctStatesTest) {
-    std::vector<std::string> cubeStatesStr {
-        "000102030405;0123456",
-        "010200030405;0123456",
-        "020100030405;0123456",
-        "000102030405;0123546",
-        "000102030405;0323546",
-        "102112032405;0323546",
+namespace {
+    class ruCubeStateParserCorrectStatesTest: public testing::TestWithParam<std::tuple<std::string, ruCubeStateVect>> {
+        public:
+            struct toString {
+                template <class ParamType>
+                std::string operator()(const testing::TestParamInfo<ParamType>& testData) const {
+                    ruCubeStateConverter conv;
+                    const auto &[cubeStateStr, vectCubeState] = testData.param;
+                    const auto &[cp, co, ep] = vectCubeState;
+
+                    std::string parsedCubeStateStr = cubeStateStr;
+                    std::replace_if(begin(parsedCubeStateStr),
+                                    end(parsedCubeStateStr),
+                                    ::ispunct,
+                                    '_');
+
+                    return parsedCubeStateStr + "_" + conv.containerToString(co) + "_" + conv.containerToString(cp);
+                }
+            };
     };
 
-    std::vector<ruCubeStateVect> expectedVectStates {
-        { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
-        { { 0, 0, 0, 0, 0, 0 }, { 1, 2, 0, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
-        { { 0, 0, 0, 0, 0, 0 }, { 2, 1, 0, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
-        { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 1, 2, 3, 5, 4, 6 } },
-        { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 3, 2, 3, 5, 4, 6 } },
-        { { 1, 2, 1, 0, 2, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 3, 2, 3, 5, 4, 6 } },
-    };
+    INSTANTIATE_TEST_SUITE_P (
+        ruCubeStateParserTests,
+        ruCubeStateParserCorrectStatesTest,
+        ::testing::ValuesIn(testDataGenerators::combine2VectorsLinear<std::string, ruCubeStateVect> (
+            {
+                "000102030405;0123456",
+                "010200030405;0123456",
+                "020100030405;0123456",
+                "000102030405;0123546",
+                "000102030405;0323546",
+                "102112032405;0323546",
+            },
+            {
+                { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
+                { { 0, 0, 0, 0, 0, 0 }, { 1, 2, 0, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
+                { { 0, 0, 0, 0, 0, 0 }, { 2, 1, 0, 3, 4, 5 }, { 0, 1, 2, 3, 4, 5, 6 } },
+                { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 1, 2, 3, 5, 4, 6 } },
+                { { 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 3, 2, 3, 5, 4, 6 } },
+                { { 1, 2, 1, 0, 2, 0 }, { 0, 1, 2, 3, 4, 5 }, { 0, 3, 2, 3, 5, 4, 6 } },
+            }
+        )),
+        ruCubeStateParserCorrectStatesTest::toString()
+    );
 
-    for (size_t i = 0; i < size(expectedVectStates); ++i) {
+    TEST_P(ruCubeStateParserCorrectStatesTest, correctStatesTest) {
+        const auto &[cubeStateStr, expected] = GetParam();
         ruCubeStateVect vectState;
-        ASSERT_NO_THROW(vectState = ruCubeStateParser::stringStateToVect(cubeStatesStr[i]));
-        ASSERT_EQ(expectedVectStates[i], vectState);
-
+        ASSERT_NO_THROW(vectState = ruCubeStateParser::stringStateToVect(cubeStateStr));
+        ASSERT_EQ(expected, vectState);
     }
 }
 
